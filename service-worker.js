7961bc30c3cc3df5dcc50109de731cbdb13515c8
vinod-desktop <katara28vinod@gmail.com>
2025-08-12 22:14:11 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.c44d3465127017d0d8525f7261b3ba6d.js"
+  "/portfolio/precache-manifest.353b5113dfa06a5a97179e9586c72a79.js"
 );
 
 self.addEventListener('message', (event) => {
